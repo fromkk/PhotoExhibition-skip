6af94b148ef078b62a1cfec5da3e5685e229d5bd
Kazuya Ueoka <info@fromkk.info>
2025-03-09 18:01:29 +0900

    implements firebase

--- Darwin/Sources/Main.swift
+++ Darwin/Sources/Main.swift
@@ -1,65 +1,71 @@
-import SwiftUI
+import FirebaseCore
 import PhotoExhibition
+import SwiftUI
 
 /// The entry point to the app simply loads the App implementation from SPM module.
 @main struct AppMain: App {
-    @AppDelegateAdaptor(AppMainDelegate.self) var appDelegate
-    @Environment(\.scenePhase) private var scenePhase
+  @AppDelegateAdaptor(AppMainDelegate.self) var appDelegate
+  @Environment(\.scenePhase) private var scenePhase
 
-    var body: some Scene {
-        WindowGroup {
-            PhotoExhibitionRootView()
-        }
-        .onChange(of: scenePhase) { oldPhase, newPhase in
-            switch newPhase {
-            case .active:
-                AppDelegate.shared.onResume(appDelegate.application)
-            case .inactive:
-                AppDelegate.shared.onPause(appDelegate.application)
-            case .background:
-                AppDelegate.shared.onStop(appDelegate.application)
-            @unknown default:
-                print("unknown app phase: \(newPhase)")
-            }
-        }
+  var body: some Scene {
+    WindowGroup {
+      PhotoExhibitionRootView()
     }
+    .onChange(of: scenePhase) { oldPhase, newPhase in
+      switch newPhase {
+      case .active:
+        AppDelegate.shared.onResume(appDelegate.application)
+      case .inactive:
+        AppDelegate.shared.onPause(appDelegate.application)
+      case .background:
+        AppDelegate.shared.onStop(appDelegate.application)
+      @unknown default:
+        print("unknown app phase: \(newPhase)")
+      }
+    }
+  }
 }
 
 typealias AppDelegate = PhotoExhibitionAppDelegate
 #if canImport(UIKit)
-typealias AppDelegateAdaptor = UIApplicationDelegateAdaptor
-typealias AppMainDelegateBase = UIApplicationDelegate
-typealias AppType = UIApplication
+  typealias AppDelegateAdaptor = UIApplicationDelegateAdaptor
+  typealias AppMainDelegateBase = UIApplicationDelegate
+  typealias AppType = UIApplication
 #elseif canImport(AppKit)
-typealias AppDelegateAdaptor = NSApplicationDelegateAdaptor
-typealias AppMainDelegateBase = NSApplicationDelegate
-typealias AppType = NSApplication
+  typealias AppDelegateAdaptor = NSApplicationDelegateAdaptor
+  typealias AppMainDelegateBase = NSApplicationDelegate
+  typealias AppType = NSApplication
 #endif
 
 class AppMainDelegate: NSObject, AppMainDelegateBase {
-    let application = AppType.shared
+  let application = AppType.shared
 
-    #if canImport(UIKit)
-    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        AppDelegate.shared.onStart(application)
-        return true
+  #if canImport(UIKit)
+    func application(
+      _ application: UIApplication,
+      willFinishLaunchingWithOptions launchOptions: [UIApplication
+        .LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+      AppDelegate.shared.onStart(application)
+      FirebaseApp.configure()
+      return true
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        AppDelegate.shared.onDestroy(application)
+      AppDelegate.shared.onDestroy(application)
     }
 
     func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
-        AppDelegate.shared.onLowMemory(application)
+      AppDelegate.shared.onLowMemory(application)
     }
-    #elseif canImport(AppKit)
+  #elseif canImport(AppKit)
     func applicationWillFinishLaunching(_ notification: Notification) {
-        AppDelegate.shared.onStart(application)
+      AppDelegate.shared.onStart(application)
     }
 
     func applicationWillTerminate(_ application: Notification) {
-        AppDelegate.shared.onDestroy(application)
+      AppDelegate.shared.onDestroy(application)
     }
-    #endif
+  #endif
 
 }
--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // This is a Skip (https://skip.tools) package,
 // containing a Swift Package Manager project
 // that will use the Skip build plugin to transpile the
@@ -7,23 +7,34 @@
 import PackageDescription
 
 let package = Package(
-    name: "photo-exhibition",
-    defaultLocalization: "en",
-    platforms: [.iOS(.v17), .macOS(.v14), .tvOS(.v17), .watchOS(.v10), .macCatalyst(.v17)],
-    products: [
-        .library(name: "PhotoExhibitionApp", type: .dynamic, targets: ["PhotoExhibition"]),
-    ],
-    dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "1.3.2"),
-        .package(url: "https://source.skip.tools/skip-ui.git", from: "1.0.0")
-    ],
-    targets: [
-        .target(name: "PhotoExhibition", dependencies: [
-            .product(name: "SkipUI", package: "skip-ui")
-        ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
-        .testTarget(name: "PhotoExhibitionTests", dependencies: [
-            "PhotoExhibition",
-            .product(name: "SkipTest", package: "skip")
-        ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
-    ]
+  name: "photo-exhibition",
+  defaultLocalization: "en",
+  platforms: [
+    .iOS(.v17), .macOS(.v14), .tvOS(.v17), .watchOS(.v10), .macCatalyst(.v17),
+  ],
+  products: [
+    .library(
+      name: "PhotoExhibitionApp", type: .dynamic, targets: ["PhotoExhibition"])
+  ],
+  dependencies: [
+    .package(url: "https://source.skip.tools/skip.git", from: "1.3.2"),
+    .package(url: "https://source.skip.tools/skip-ui.git", from: "1.0.0"),
+    .package(url: "https://source.skip.tools/skip-firebase.git", from: "0.7.0"),
+  ],
+  targets: [
+    .target(
+      name: "PhotoExhibition",
+      dependencies: [
+        .product(name: "SkipUI", package: "skip-ui"),
+        .product(name: "SkipFirebaseAuth", package: "skip-firebase"),
+      ], resources: [.process("Resources")],
+      plugins: [.plugin(name: "skipstone", package: "skip")]),
+    .testTarget(
+      name: "PhotoExhibitionTests",
+      dependencies: [
+        "PhotoExhibition",
+        .product(name: "SkipTest", package: "skip"),
+      ], resources: [.process("Resources")],
+      plugins: [.plugin(name: "skipstone", package: "skip")]),
+  ]
 )
