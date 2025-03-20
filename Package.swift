32a244df29f852d05fc4606a4861daf10bed29f8
Kazuya Ueoka <info@fromkk.info>
2025-03-20 18:54:54 +0900

    fix dependency url

--- Package.swift
+++ Package.swift
@@ -17,10 +17,13 @@ let package = Package(
       name: "PhotoExhibitionApp", type: .dynamic, targets: ["PhotoExhibition"])
   ],
   dependencies: [
-    .package(url: "https://source.skip.tools/skip.git", from: "1.3.4"),
-    .package(url: "https://source.skip.tools/skip-ui.git", from: "1.26.7"),
-    .package(url: "https://source.skip.tools/skip-kit.git", from: "0.3.1"),
-    .package(url: "https://source.skip.tools/skip-firebase.git", from: "0.7.3"),
+    .package(url: "https://github.com/skiptools/skip.git", from: "1.3.4"),
+    .package(url: "https://github.com/skiptools/skip-lib", from: "1.3.4"),
+    .package(url: "https://github.com/skiptools/skip-model", from: "1.4.2"),
+    .package(url: "https://github.com/skiptools/skip-ui.git", from: "1.26.7"),
+    .package(url: "https://github.com/skiptools/skip-unit.git", from: "1.1.1"),
+    .package(url: "https://github.com/skiptools/skip-kit.git", from: "0.3.1"),
+    .package(url: "https://github.com/skiptools/skip-firebase.git", from: "0.7.3"),
   ],
   targets: [
     .target(
@@ -46,3 +49,4 @@ let package = Package(
     ),
   ]
 )
+
