8371c0968206bf14225a6329a9428b01769bbfa7
Kazuya Ueoka <info@fromkk.info>
2025-03-21 08:33:21 +0900

    fix package source

--- Package.swift
+++ Package.swift
@@ -17,10 +17,10 @@ let package = Package(
       name: "PhotoExhibitionApp", type: .dynamic, targets: ["PhotoExhibition"])
   ],
   dependencies: [
-    .package(url: "https://github.com/skiptools/skip.git", from: "1.3.4"),
-    .package(url: "https://github.com/skiptools/skip-ui.git", from: "1.26.7"),
-    .package(url: "https://github.com/skiptools/skip-kit.git", from: "0.3.1"),
-    .package(url: "https://github.com/skiptools/skip-firebase.git", from: "0.7.3"),
+    .package(url: "https://source.skip.tools/skip.git", from: "1.3.4"),
+    .package(url: "https://source.skip.tools/skip-ui.git", from: "1.26.7"),
+    .package(url: "https://source.skip.tools/skip-kit.git", from: "0.3.1"),
+    .package(url: "https://source.skip.tools/skip-firebase.git", from: "0.7.3"),
   ],
   targets: [
     .target(
