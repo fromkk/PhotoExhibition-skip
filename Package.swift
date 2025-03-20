10b34642872b7096ba909869d5518be4e166ad21
Kazuya Ueoka <info@fromkk.info>
2025-03-20 18:45:06 +0900

    fix packages

--- Package.swift
+++ Package.swift
@@ -17,11 +17,10 @@ let package = Package(
       name: "PhotoExhibitionApp", type: .dynamic, targets: ["PhotoExhibition"])
   ],
   dependencies: [
-    .package(url: "https://source.skip.tools/skip.git", from: "1.3.3"),
-    .package(url: "https://source.skip.tools/skip-ui.git", from: "1.26.4"),
-    .package(url: "https://github.com/skiptools/skip-kit.git", from: "0.3.1"),
+    .package(url: "https://source.skip.tools/skip.git", from: "1.3.4"),
+    .package(url: "https://source.skip.tools/skip-ui.git", from: "1.26.7"),
+    .package(url: "https://source.skip.tools/skip-kit.git", from: "0.3.1"),
     .package(url: "https://source.skip.tools/skip-firebase.git", from: "0.7.3"),
-    .package(url: "https://source.skip.tools/skip-web.git", from: "0.5.0"),
   ],
   targets: [
     .target(
@@ -33,7 +32,6 @@ let package = Package(
         .product(name: "SkipFirebaseFirestore", package: "skip-firebase"),
         .product(name: "SkipFirebaseFunctions", package: "skip-firebase"),
         .product(name: "SkipFirebaseStorage", package: "skip-firebase"),
-        .product(name: "SkipWeb", package: "skip-web"),
       ],
       resources: [.process("Resources")],
       plugins: [.plugin(name: "skipstone", package: "skip")]),
--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+set -e
+defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+
 brew install skiptools/skip/skip
