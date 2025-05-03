9e32fdb8e3757f3a725860a3d20decd0b5b06a7e
Kazuya Ueoka <info@fromkk.info>
2025-05-03 19:12:51 +0900

    show app versions

--- Darwin/PhotoExhibitionViewer/Views/Menu/MenuView.swift
+++ Darwin/PhotoExhibitionViewer/Views/Menu/MenuView.swift
@@ -3,6 +3,11 @@ import Viewer
 
 @Observable
 final class MenuStore: Store {
+  let deviceInfoClient: DeviceInfoClient
+  init(deviceInfoClient: DeviceInfoClient = .liveValue) {
+    self.deviceInfoClient = deviceInfoClient
+  }
+
   enum Action {
     case licenseButtonTapped
   }
@@ -25,39 +30,49 @@ struct MenuView: View {
   var body: some View {
     NavigationStack {
       List {
-        Button {
-          openURL(Constants.termsOfServiceURL)
-        } label: {
-          HStack {
-            Text("Terms of Service")
-              .frame(maxWidth: .infinity, alignment: .leading)
+        Section {
+          Button {
+            openURL(Constants.termsOfServiceURL)
+          } label: {
+            HStack {
+              Text("Terms of Service")
+                .frame(maxWidth: .infinity, alignment: .leading)
 
-            Image(systemName: "rectangle.on.rectangle")
+              Image(systemName: "rectangle.on.rectangle")
+            }
           }
-        }
 
-        Button {
-          openURL(Constants.privacyPolicyURL)
-        } label: {
-          HStack {
-            Text("Privacy Policy")
-              .frame(maxWidth: .infinity, alignment: .leading)
+          Button {
+            openURL(Constants.privacyPolicyURL)
+          } label: {
+            HStack {
+              Text("Privacy Policy")
+                .frame(maxWidth: .infinity, alignment: .leading)
 
-            Image(systemName: "rectangle.on.rectangle")
+              Image(systemName: "rectangle.on.rectangle")
+            }
           }
-        }
 
-        Button {
-          store.send(.licenseButtonTapped)
-        } label: {
-          HStack {
-            Text("Licenses")
-              .frame(maxWidth: .infinity, alignment: .leading)
-            Image(systemName: "chevron.forward")
+          Button {
+            store.send(.licenseButtonTapped)
+          } label: {
+            HStack {
+              Text("Licenses")
+                .frame(maxWidth: .infinity, alignment: .leading)
+              Image(systemName: "chevron.forward")
+            }
           }
-        }
-        .navigationDestination(item: $store.licenseStore) { licenstListStore in
-          LicenseListView(store: licenstListStore)
+          .navigationDestination(item: $store.licenseStore) { licenstListStore in
+            LicenseListView(store: licenstListStore)
+          }
+        } footer: {
+          if let version = store.deviceInfoClient.appVersion(),
+            let buildNumber = store.deviceInfoClient.buildNumber()
+          {
+            Text("\(version) (\(buildNumber))")
+              .font(.footnote)
+              .frame(maxWidth: .infinity, alignment: .center)
+          }
         }
       }
       .navigationBarTitle("Menu")
