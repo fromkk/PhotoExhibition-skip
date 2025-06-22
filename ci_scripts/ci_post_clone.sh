227edecd78ffd474ea67bfda788ec0cc831078e7
Kazuya Ueoka <info@fromkk.info>
2025-06-22 09:48:54 +0900

    ci: disable gradle during CI

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,3 +5,6 @@ defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
 
 brew install skiptools/skip/skip
+
+# Disable Skip during CI builds by preventing gradle from running
+echo "SKIP_ACTION = none" >> Darwin/PhotoExhibition.xcconfig
